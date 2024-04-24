ac855dd5586c11435778d3aeffceafb2a2e5e697
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <leg.gwenaelle@gmail.com>
2024-04-24 11:53:34 +0200

    Add: delete button for Input in InputPage and OneInputPage

--- src/pages/Inputs/InputsPage.jsx
+++ src/pages/Inputs/InputsPage.jsx
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom";
 import { useInput } from "../../context/InputContext";
 
 function InputsPage() {
-  const { inputsData, fetchAllInputs } = useInput();
+  const { inputsData, fetchAllInputs, deleteInput } = useInput();
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [deletedId, setDeletedId] = useState(null);
+  const [showConfirmation, setShowConfirmation] = useState(false);
 
   useEffect(() => {
     async function fetchInputs() {
@@ -21,24 +23,39 @@ function InputsPage() {
       }
     }
     fetchInputs();
-  }, [fetchAllInputs]);
+  }, [fetchAllInputs, deletedId]);
 
   if (loading) return <h3>Loading...</h3>;
   if (error) return <h3>{error}</h3>;
   if (!inputsData) return <h3>No inputs to see, please add some</h3>;
 
+  const handleDelete = async (inputId) => {
+    if (window.confirm("Are you sure you want to delete this input?")) {
+      try {
+        await deleteInput(inputId);
+        setDeletedId(inputId);
+        setShowConfirmation(true);
+        setTimeout(() => {
+          setShowConfirmation(false);
+        }, 2000);
+      } catch (error) {
+        console.error("Failed to delete input", error);
+        setError("Failed to delete input");
+      }
+    }
+  };
+
   return (
     <div>
-      {inputsData.map((input) => {
-        return (
-          <div key={input._id}>
-            <h2>
-              {/* <Link to={`/inputs/${input.date}`}>{input.date}</Link> */}
-              <Link to={`/inputs/${input._id}`}>{input.date}</Link>
-            </h2>
-          </div>
-        );
-      })}
+      {showConfirmation && <h4>Input deleted successfully!</h4>}
+      {inputsData.map((input) => (
+        <div key={input._id}>
+          <h2>
+            <Link to={`/inputs/${input._id}`}>{input.date}</Link>
+          </h2>
+          <button onClick={() => handleDelete(input._id)}>Remove</button>
+        </div>
+      ))}
     </div>
   );
 }
