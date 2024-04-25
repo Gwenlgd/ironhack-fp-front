f3cd3d28df99be3b726dac215d0519213890733e
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <leg.gwenaelle@gmail.com>
2024-04-25 10:28:57 +0200

    Added basic styling to symptoms in form

--- src/pages/Symptoms/Symptoms.jsx
+++ src/pages/Symptoms/Symptoms.jsx
@@ -23,17 +23,31 @@ const Symptoms = () => {
 
   return (
     <fieldset>
-      <legend>Symptoms</legend>
-      {symptoms.map((symptom) => (
-        <label key={symptom._id}>
-          <input
-            type="checkbox"
-            checked={selectedSymptoms.includes(symptom._id)}
-            onChange={() => handleSymptomChange(symptom._id)}
-          />
-          {symptom.name}
-        </label>
-      ))}
+      <legend className="mb-5 text-lg font-medium text-gray-900 dark:text-white">
+        Symptoms
+      </legend>
+      <ul className="grid grid-cols-2 md:grid-cols-3 gap-6 w-full">
+        {symptoms.map((symptom) => (
+          <li key={symptom._id}>
+            <input
+              type="checkbox"
+              className="hidden peer"
+              checked={selectedSymptoms.includes(symptom._id)}
+              onChange={() => handleSymptomChange(symptom._id)}
+            />
+            <label
+              htmlFor={symptom._id}
+              className="inline-flex flex-col items-center justify-center w-full h-24 p-3 text-center text-gray-500 bg-white border-2 border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 peer-checked:border-blue-600 peer-checked:bg-blue-50 hover:text-gray-600 dark:peer-checked:text-blue-200 peer-checked:text-blue-600 hover:bg-gray-50 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700"
+            >
+              <div className="block">
+                <div className="w-full text-lg font-semibold">
+                  {symptom.name}
+                </div>
+              </div>{" "}
+            </label>
+          </li>
+        ))}
+      </ul>
     </fieldset>
   );
 };
