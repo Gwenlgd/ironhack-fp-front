cd885e2cb88b0b771382e58c0c8abc0aef6f2dd4
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <leg.gwenaelle@gmail.com>
2024-04-25 10:24:49 +0200

    Add layout container component

---
+++ src/components/Layout/LayoutContainer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LayoutContainer = ({ children }) => {
+  return <div className="mx-4 sm:mx-6 md:mx-10 lg:mx-20">{children}</div>;
+};
+
+export default LayoutContainer;
